1f997ed5fa7d023e19ea131d659ad8910633e500
Tamas Szabo <szabogtamas@gmail.com>
2021-02-20 18:15:56 +0100

    Fixed copytree version error

--- scripts/ma_redifined_enrichment.r
+++ scripts/ma_redifined_enrichment.r
@@ -69,6 +69,38 @@ plot_metabo_enrichment <- function(in_df){
   
   in_df %>%
     Enrich.Anal()
+
+    # Create mSetObj for storing objects created during your analysis
+mSet<-InitDataObjects("conc", "pathora", FALSE)
+# Set up mSetObj with the list of compounds
+mSet<-Setup.MapData(mSet, tmp.vec);
+# Cross reference list of compounds against libraries (hmdb, pubchem, chebi, kegg, metlin)
+mSet<-CrossReferencing(mSet, "name");
+# Creates a mapping result table; shows HMDB, KEGG, PubChem, etc. IDs
+# Saved as "name_map.csv" or can be found in mSet$dataSet$map.table
+# Compounds with no hits will contain NAs across the columns
+mSet<-CreateMappingResultTable(mSet);
+# From the mapping result table, L-Isolucine has no matches
+# Now, perform potential matching with our database against this compound
+mSet<-PerformDetailMatch(mSet, "L-Isolucine");
+# Get list of candidates for matching
+# Results are found in mSet$name.map$hits.candidate.list
+mSet<-GetCandidateList(mSet);
+# Replace L-Isolucine with selected compound (L-Isoleucine)
+mSet<-SetCandidate(mSet, "L-Isolucine", "L-Isoleucine");
+
+# Use "current" for the latest KEGG pathway library or "v2018" for the KEGG pathway library version prior to November 2019.
+mSet<-SetKEGG.PathLib(mSet, "hsa", "current")
+# Set the metabolite filter
+# Default set to false
+mSet<-SetMetabolomeFilter(mSet, F);
+# Calculate the over representation analysis score, here we selected to use the hypergeometric test (alternative is Fisher's exact test)
+# A results table "pathway_results.csv" will be created and found within your working directory
+mSet<-CalculateOraScore(mSet, "rbc", "hyperg")
+# Plot of the Pathway Analysis Overview
+mSet<-PlotPathSummary(mSet, "path_view_0_", "png", 72, width=NA)
+# Plot a specific metabolic pathway, in this case "Glycine, serine and threonine metabolism"
+mSet<-PlotKEGGPath(mSet, "Glycine, serine and threonine metabolism",528, 480, "png", NULL)
   
 }
 
